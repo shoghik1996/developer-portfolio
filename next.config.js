873762fb20c621472acbf4591f8ecfd54ea220ad
shoghik1996 <shoghikmartirosyan96@gmail.com>
2024-10-11 05:23:41 -0400

    unoptimized

--- next.config.js
+++ next.config.js
@@ -1,14 +1,15 @@
-const path = require('path')
- 
+const path = require('path');
+
 module.exports = {
   basePath: "/developer-portfolio",
-  assetPrefix: '/developer-portfolio/',  
-  output: "export",  // <=== enables static exports
+  assetPrefix: '/developer-portfolio/',
+  output: "export", // Enables static exports
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
   images: {
+    unoptimized: true, // Disable Next.js Image Optimization for static exports
     remotePatterns: [
       {
         protocol: 'https',
@@ -22,4 +23,4 @@ module.exports = {
       },
     ],
   },
-}
+};
