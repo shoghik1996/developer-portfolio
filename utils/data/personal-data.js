93168049e50b96cb3e32ed96c69abec3276f4132
shoghik1996 <shoghikmartirosyan96@gmail.com>
2024-10-11 08:19:13 -0400

    update address

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -5,7 +5,7 @@ export const personalData = {
   description: "My name is Shoghik Martirosyan. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
   email: 'shoghikmartirosyan96@gmail.com',
   phone: '+37477435523',
-  address: 'Middle Badda, Dhaka, Bangladesh - 1212 ',
+  address: 'Aragacotni marz Irind 26/4, Yerevan, Armenia - 0507 ',
   github: 'https://github.com/shoghik1996',
   facebook: 'https://www.facebook.com/shoghik1996/',
   linkedIn: 'https://www.linkedin.com/in/shoghik1996/',
