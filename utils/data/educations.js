88fc8c571c9f5aa6f60dfcc3c20cd6ecd94630e7
shoghik1996 <shoghikmartirosyan96@gmail.com>
2024-10-11 06:03:45 -0400

    update education

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -1,20 +1,20 @@
 export const educations = [
   {
     id: 1,
-    title: "Bachelor Degree",
-    duration: "2020 - Present",
-    institution: "National University of Bangladesh",
+    title: "Bachelor of Science in Computer Science",
+    duration: "2014 - 2018",
+    institution: "Yerevan State University",
   },
   {
     id: 2,
-    title: "Higher Secondary Certificate",
-    duration: "2018 - 2020",
-    institution: "Noakhali Islamia Kamil Madrasah",
+    title: "High School Diploma",
+    duration: "2010 - 2014",
+    institution: "Anania Shirakatsi Lyceum",
   },
   {
     id: 3,
-    title: "Secondary School Certificate",
-    duration: "2008 - 2018",
-    institution: "Baitus Saif Islamia Madrasah",
+    title: "Middle School",
+    duration: "2004 - 2010",
+    institution: "Yerevan Basic School No. 132",
   }
-]
+];
