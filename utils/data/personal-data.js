da746b6c20d03f49f2dbaa96e04e8a2d824e787c
shoghik1996 <shoghikmartirosyan96@gmail.com>
2024-10-11 04:20:55 -0400

    first commit

---
+++ utils/data/personal-data.js
@@ -0,0 +1,17 @@
+export const personalData = {
+  name: "Shoghik Martirosyan",
+  profile: '/profile.png',
+  designation: "Software Developer",
+  description: "My name is Shoghik Martirosyan. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
+  email: 'shoghikmartirosyan96@gmail.com',
+  phone: '+37477435523',
+  address: 'Middle Badda, Dhaka, Bangladesh - 1212 ',
+  github: 'https://github.com/shoghik1996',
+  facebook: 'https://www.facebook.com/shoghik1996/',
+  linkedIn: 'https://www.linkedin.com/in/shoghik1996/',
+  twitter: 'https://twitter.com/shoghik1996',
+  stackOverflow: 'https://stackoverflow.com/users/16840768/shoghik1996',
+  leetcode: "https://leetcode.com/shoghik1996/",
+  devUsername: "shoghik1996",
+  resume: "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing"
+}
